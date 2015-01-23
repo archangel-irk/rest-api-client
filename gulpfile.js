fc9053d6f311e67b3a39c0c541dc70327f9cb152
Constantine Melnikov <ka.melnikov@gmail.com>
2015-01-23 15:09:46 +0800

    add karma and start_nodejs_server to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,8 @@ var gulp = require('gulp');
 var browserify = require('gulp-browserify');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
+var server = require('gulp-develop-server');
+var karma = require('karma').server;
 
 gulp.task('build:debug', function() {
   gulp.src('src/index.js')
@@ -18,7 +20,7 @@ gulp.task('build:debug', function() {
       debug: true
     }) )
     .pipe( rename('api-client.js') )
-    .pipe( gulp.dest('dist') )
+    .pipe( gulp.dest('dist') );
 });
 
 gulp.task('build', function() {
@@ -28,7 +30,37 @@ gulp.task('build', function() {
     }) )
     .pipe( uglify() )
     .pipe( rename('api-client.min.js') )
-    .pipe( gulp.dest('dist') )
+    .pipe( gulp.dest('dist') );
+});
+
+/*gulp.task('server', function () {
+  // Start the server at the beginning of the task
+  server.run({
+    file: 'tests/server/app.js'
+  });
+
+  server.stop();
+});*/
+
+gulp.task('server', function() {
+  server.listen({
+    path: 'tests/server/app.js'
+  });
+
+  setTimeout(function(){
+    server.kill();
+  }, 10000 );
+});
+
+gulp.task('test', ['server'], function (done) {
+  karma.start({
+    configFile: __dirname + '/karma.conf.js'
+  }, function(){
+
+    server.kill();
+
+    done();
+  });
 });
 
 gulp.task('default', ['build', 'build:debug']);
