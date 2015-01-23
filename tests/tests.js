12682838d55368b0c1dbe6268bfdc86b099a09b0
Constantine Melnikov <ka.melnikov@gmail.com>
2015-01-23 15:11:03 +0800

    update tests

--- tests/tests.js
+++ tests/tests.js
@@ -7,8 +7,50 @@
 (function(){
 'use strict';
 
-QUnit.test('hello test', function( assert ) {
+/*QUnit.test('hello test', function( assert ) {
   assert.ok( 1 == '1', 'Passed!' );
+
+  var server = sinon.fakeServer.create();
+
+  server.respondWith(function(xhr){
+    console.log("before request.respond()");
+
+    xhr.respond(200, null, "Hello world");
+
+    console.log("after request.respond()");
+  });
+
+  var github = ApiClient('https://api.github.com', {
+    hooks: {
+      headers: {
+        Accept: 'application/vnd.github.v3+json',
+        Authorization: 'token e31ba2b2c80dfec76606d3a636e722fe4db0e64c'
+      }
+    },
+    unauthorizedCallback: function(){}
+  });
+
+  window.github = github;
+
+  console.log("before make request");
+
+  github.read().done(function( result ){
+    console.log( result );
+
+    assert.equal(result, 'Hello world', "id was extracted from the response object");
+  });
+
+  console.log("before server.respond()");
+
+  server.respond();
+
+  console.log("after server.respond()");
+});*/
+
+describe('collection', function(){
+  it('create', function(done){
+    done();
+  });
 });
 
 })();
