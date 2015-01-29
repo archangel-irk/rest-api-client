5ea4490039fdc8351c88e469cf35ed9a4b360ceb
Constantine Melnikov <ka.melnikov@gmail.com>
2015-01-29 18:49:57 +0800

    added xhr (to get rid of dependencies jquery) in future

---
+++ src/xhr.js
@@ -0,0 +1,11 @@
+/**
+ * User: Constantine Melnikov
+ * Email: ka.melnikov@gmail.com
+ * Date: 29.01.15
+ * Time: 17:16
+ */
+'use strict';
+
+function createXhr() {
+    return new window.XMLHttpRequest();
+}
