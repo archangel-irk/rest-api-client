4586954a72150afad7ed4059590c943bb8370dc4
Konstantin Melnikov <ka.melnikov@gmail.com>
2019-07-19 14:24:00 +0800

    add terser conf ftom Polymer

--- rollup.config.js
+++ rollup.config.js
@@ -9,6 +9,15 @@ export default {
     banner: "/** Rest-Api-Client - (c) Constantine Melnikov 2013 - 2019 - MIT Licensed */",
   },
   plugins: [
+    // terser({
+    //   warnings: true,
+    //   module: true,
+    //   mangle: {
+    //     properties: {
+    //       regex: /^__/,
+    //     },
+    //   },
+    // }),
     // terser(), // минификатор совместимый с ES2015+, форк и наследник UglifyES,
     filesize({
       showBrotliSize: true,
