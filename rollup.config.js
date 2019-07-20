5cf1ecf43626af7a78ee02731a3f9e2e741ef3ae
Konstantin Melnikov <ka.melnikov@gmail.com>
2019-07-20 18:40:15 +0800

    discount CommonJS bundles

--- rollup.config.js
+++ rollup.config.js
@@ -64,9 +64,6 @@ function generateBundleConfig(outputFile, format, mode) {
 }
 
 export default [
-  generateBundleConfig(path.join('dist', 'api-client.cjs.development.js'), Format.CommonJS, Mode.DEVELOPMENT),
-  generateBundleConfig(path.join('dist', 'api-client.cjs.production.min.js'), Format.CommonJS, Mode.PRODUCTION),
-
   generateBundleConfig(path.join('dist', 'api-client.esm.development.js'), Format.ESModule, Mode.DEVELOPMENT),
   generateBundleConfig(path.join('dist', 'api-client.esm.production.min.js'), Format.ESModule, Mode.PRODUCTION),
 
