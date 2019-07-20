46b1cd160fc250cf96660b3d6657689657293e1b
Konstantin Melnikov <ka.melnikov@gmail.com>
2019-07-20 18:59:52 +0800

    code style

--- test/api-client.test.js
+++ test/api-client.test.js
@@ -1,112 +1,111 @@
-describe('ApiClient', function(){
+describe('ApiClient', function() {
   //this.timeout( 0 );
 
-  it('create without `new`', function(  ){
-    assert.ok( ApiClient() instanceof ApiClient );
+  it('create without `new`', function() {
+    assert.ok(ApiClient() instanceof ApiClient);
   });
 
-  it('read root .done()', function( done ){
+  it('read root .done()', function(done) {
     var api = new ApiClient('http://0.0.0.0:3000');
 
-    api.read().done(function( data ){
-      assert.equal( data.user, 'tobi' );
+    api.read().done(function(data) {
+      assert.equal(data.user, 'tobi');
 
       done();
     });
   });
 
-  it('read root with callback', function( done ){
+  it('read root with callback', function(done) {
     var api = new ApiClient('http://0.0.0.0:3000');
 
-    api.read(function( data ){
-      assert.equal( data.user, 'tobi' );
+    api.read(function(data) {
+      assert.equal(data.user, 'tobi');
 
       done();
     });
   });
 
-  describe('GET', function(){
-    it('users with resource function call and .done()', function( done ){
+  describe('GET', function() {
+    it('users with resource function call and .done()', function(done) {
       var api = new ApiClient('http://0.0.0.0:3000');
 
       api.add('users');
 
-      api.users().read().done(function( data ){
-        assert.deepEqual( data, [{ user: 'tobi' },{ user: 'loki' }] );
+      api.users().read().done(function(data) {
+        assert.deepEqual(data, [{ user: 'tobi' }, { user: 'loki' }]);
 
         done();
       });
     });
 
-    it('users without resource function call and .done()', function( done ){
+    it('users without resource function call and .done()', function(done) {
       var api = new ApiClient('http://0.0.0.0:3000');
 
       api.add('users');
 
-      api.users.read().done(function( data ){
-        assert.deepEqual( data, [{ user: 'tobi' },{ user: 'loki' }] );
+      api.users.read().done(function(data) {
+        assert.deepEqual(data, [{ user: 'tobi' }, { user: 'loki' }]);
 
         done();
       });
     });
 
-    it('users with resource function call and callback', function( done ){
+    it('users with resource function call and callback', function(done) {
       var api = new ApiClient('http://0.0.0.0:3000');
 
       api.add('users');
 
-      api.users().read(function( data ){
-        assert.deepEqual( data, [{ user: 'tobi' },{ user: 'loki' }] );
+      api.users().read(function(data) {
+        assert.deepEqual(data, [{ user: 'tobi' }, { user: 'loki' }]);
 
         done();
       });
     });
 
-    it('users without resource function call and callback', function( done ){
+    it('users without resource function call and callback', function(done) {
       var api = new ApiClient('http://0.0.0.0:3000');
 
       api.add('users');
 
-      api.users.read(function( data ){
-        assert.deepEqual( data, [{ user: 'tobi' },{ user: 'loki' }] );
+      api.users.read(function(data) {
+        assert.deepEqual(data, [{ user: 'tobi' }, { user: 'loki' }]);
 
         done();
       });
     });
   });
 
-
-  describe('POST', function(){
-    it('users', function( done ){
+  describe('POST', function() {
+    it('users', function(done) {
       var api = new ApiClient('http://0.0.0.0:3000');
 
       api.add('users');
 
       api.users.post({
         username: 'login',
-        password: 'password'
-      }).done(function( data ){
-        assert.deepEqual( data, { username: 'login', password: 'password' });
+        password: 'password',
+      }).done(function(data) {
+        assert.deepEqual(data, { username: 'login', password: 'password' });
 
         done();
       });
     });
   });
 
-  describe('cache', function(){
-    it('works', function( done ){
+  describe('cache', function() {
+    it('works', function(done) {
       var api = new ApiClient('http://0.0.0.0:3000');
 
       api.add('users');
 
-      api.users.get().done(function( response ){
-        assert.deepEqual( response, [{ user: 'tobi' },{ user: 'loki' }]);
+      api.users.get().done(function(response) {
+        assert.deepEqual(response, [{ user: 'tobi' }, { user: 'loki' }]);
 
-        api.users.get().done(function( response ){
-          var resArr = [{ user: 'tobi' },{ user: 'loki' }];
+        api.users.get().done(function(response) {
+          var resArr = [{ user: 'tobi' }, { user: 'loki' }];
           resArr.__cached = true;
 
-          assert.deepEqual( response, resArr );
+          assert.deepEqual(response, resArr);
 
           done();
         });
