5223b3cbcbccfd6cd6112b42b4d06a4bc893ee8c
Konstantin Melnikov <ka.melnikov@gmail.com>
2019-07-20 20:22:15 +0800

    delete karma defaults

--- karma.conf.js
+++ karma.conf.js
@@ -58,16 +58,8 @@ module.exports = function (config) {
       dir: 'test/coverage',
     },
 
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
-
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
     singleRun: true,
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR
-    // || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
   });
 };
